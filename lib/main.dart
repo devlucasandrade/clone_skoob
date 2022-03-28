15d7ec1dbc4cec2267a7dfe98bd3bc553460063c
lucasandrade1981 <devlucasandrade@gmail.com>
2022-03-27 22:21:41 -0300

    Novo ajuste

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:clone_books/pages/enter_page.dart';
 import 'package:clone_books/pages/onboarding.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 bool? seenOnboard;
